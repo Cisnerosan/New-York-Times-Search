60e9e8d07e98ec5ea2c34f1479afec61d54b6571
Alba Cisneros <cisnerosan86@gmail.com>
2018-10-26 22:44:11 -0500

    info updated

---
+++ server.js
@@ -0,0 +1,52 @@
+// Dependecies
+const express = require("express");
+const mongoose = require("mongoose");
+const bluebird = require("bluebird");
+const bodyParser = require("body-parser");
+const path = require("path");
+
+const PORT = process.env.PORT || 3001;
+mongoose.Promise = bluebird;
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+} else {
+  app.use(express.static(__dirname + "/client/public"));
+}
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+  next();
+});
+
+// Routing
+var articlesController = require("./server/controllers/article-controller");
+var router = new express.Router();
+router.get("/api/saved", articlesController.find);
+router.post("/api/saved", articlesController.insert);
+router.delete("/api/saved/:id", articlesController.delete);
+router.get("/*", function(req, res) {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
+
+app.use(router);
+
+// Connect mongoose to our database
+const db = process.env.MONGODB_URI || "mongodb://localhost/New-York-Times-Search";
+mongoose.connect(db, function(error) {
+  // Log any errors connecting with mongoose
+  if (error) {
+    console.error(error);
+  }
+  else {
+    console.log("mongoose connection is successful");
+  }
+});
+
+app.listen(PORT, function() {
+  console.log(` ==> Server now on port ${PORT}!`);
+});
